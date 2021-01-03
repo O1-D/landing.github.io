a9f2e217326f28e34562bfa63d5cd0cfdb52e250
O1-D <67709733+O1-D@users.noreply.github.com>
2021-01-03 11:45:45 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+alert("Javascript подключен");
+<script src="http://maps.googleapis.com/maps/api/js?key=YOUR_KEY&callback=myMap"></script>;
+function myMap() {
+	var mapCanvas = document.getElementById("map");
+	var mapOptions = {
+		center: new google.maps.LatLng(51.5, -0.2),
+		zoom 10
+	};
+	var map = new google.maps.Map(mapCanvas, mapOptions);
+}
